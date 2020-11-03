ab74031549e61e40cbc176fa533c0726312d39f6
szendrattila <attila8000a@gmail.com>
2020-11-03 17:36:48 +0000

    Initial commit

---
+++ f0004b.py
@@ -0,0 +1,8 @@
+név = input('Hogy hívják önt? ')
+kor = input('És hány éves maga? ') 
+kor = int(kor)
+év = input('Milyen év van?')
+év = int(év)
+születési_év = év - kor
+érettségi = születési_év + 18
+print(név, ', ön ', születési_év, '-ban született.''aki' ,érettségi ,'ban érettségizett érettségizni fog. ', sep='')
---
+++ f0004d.py
@@ -0,0 +1,5 @@
+mérföld = input ('hány magyar mérföldre van a sárkány barlangja?')
+mérföld = int(mérföld)
+km = mérföld * 8.35
+print ('ez a táv', km, 'kilométer és', mérföld ,'tengeri mérföldre van')
+#ezt nem tudtam... :(
